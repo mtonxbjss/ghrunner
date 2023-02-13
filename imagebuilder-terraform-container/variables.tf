f2ce211d3321d5f5d9b39ecc8dc614d5c9b66061
Mark Tonks <mark.tonks@bjss.com>
2023-02-13 14:18:23 +0000

    Input variable validation for terraform-container module

--- imagebuilder-terraform-container/variables.tf
+++ imagebuilder-terraform-container/variables.tf
@@ -2,6 +2,10 @@ variable "container_build_pipeline_cron_expression" {
   type        = string
   description = "The cron schedule expression for when the container should be rebuilt. Defaults to 4am MON-FRI"
   default     = "cron(0 4 ? * MON-FRI *)"
+  validation {
+    condition     = can(regex("^cron\\(.*\\)$", var.container_build_pipeline_cron_expression))
+    error_message = "Invalid cron expression. A valid cron expression must have 5 fields separated by commas and each field must be a number between 0 and 59 or an asterisk (*)"
+  }
 }
 
 variable "container_sharing_account_id_list" {
@@ -14,12 +18,20 @@ variable "container_version_number" {
   type        = string
   description = "Sematic versioning version number of the container to be created. Defaults to 1.0.0"
   default     = "1.0.0"
+  validation {
+    condition     = can(regex("^(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)$", var.container_version_number))
+    error_message = "Invalid semantic version. A valid semantic version must contain three dot-separated numbers, with no leading zeros"
+  }
 }
 
 variable "ecr_private_repository_account_id" {
   type        = string
   description = "The AWS account ID that hosts the private ECR registry for job docker images. Defaults to empty (i.e. no private repository required)"
   default     = ""
+  validation {
+    condition     = can(regex("^([0-9]{12})?$", var.ecr_private_repository_account_id))
+    error_message = "Invalid account id. A valid account ID must contain 12 digits"
+  }
 }
 
 variable "ecr_private_repository_name" {
@@ -31,18 +43,26 @@ variable "ecr_private_repository_name" {
 variable "iam_roles_with_admin_access_to_created_resources" {
   type        = list(string)
   description = "List of IAM Role ARNs that should have admin access to any resources created in this module that have resource policies"
-}
-
-variable "kms_deletion_window_in_days" {
-  type        = number
-  description = "The number of days to retain a KMS key scheduled for deletion. Defaults to 7"
-  default     = 7
+  validation {
+    condition = length([
+      for arn in var.iam_roles_with_admin_access_to_created_resources :
+      true if can(regex("^arn:aws:iam::[0-9]+:.*$", arn))
+    ]) == length(var.iam_roles_with_admin_access_to_created_resources)
+    error_message = "Invalid Amazon Resource Name. A valid IAM Role ARN must start with 'arn:aws:iam', followed by a region, account ID and resource name separated by colons."
+  }
 }
 
 variable "imagebuilder_ec2_extra_security_groups" {
   type        = list(string)
   description = "List of security group IDs to append to the temporary EC2 instance that will be created in order to generate the AMI. Defaults to an empty list"
   default     = []
+  validation {
+    condition = length([
+      for sg in var.imagebuilder_ec2_extra_security_groups :
+      true if can(regex("^sg-[0-9a-f]{17}$", sg))
+    ]) == length(var.imagebuilder_ec2_extra_security_groups)
+    error_message = "Invalid security group ID. A valid security group ID must start with 'sg-' followed by 17 alphanumeric characters."
+  }
 }
 
 variable "imagebuilder_ec2_instance_type" {
@@ -54,6 +74,10 @@ variable "imagebuilder_ec2_instance_type" {
 variable "imagebuilder_log_bucket_encryption_key_arn" {
   type        = string
   description = "Encryption key ARN for the bucket that stores logs from the EC2 ImageBuilder process"
+  validation {
+    condition     = can(regex("^arn:aws:kms:[a-z0-9-]+:[0-9]+:.*$", var.imagebuilder_log_bucket_encryption_key_arn))
+    error_message = "Invalid Amazon Resource Name. A valid KMS ARN must start with 'arn:aws:kms', followed by a region, account ID and resource name separated by colons."
+  }
 }
 
 variable "imagebuilder_log_bucket_name" {
@@ -70,11 +94,19 @@ variable "imagebuilder_ec2_root_volume_size" {
   type        = number
   description = "Size of root volume for the temporary EC2 instance that will be created in order to generate the AMI. Defaults to 100GiB"
   default     = 100
+  validation {
+    condition     = var.imagebuilder_ec2_root_volume_size == floor(var.imagebuilder_ec2_root_volume_size)
+    error_message = "The variable must be a whole integer"
+  }
 }
 
 variable "imagebuilder_ec2_subnet_id" {
   type        = string
   description = "ID of the subnet used to host the temporary EC2 instance that will be created in order to generate the AMI"
+  validation {
+    condition     = can(regex("^subnet-[a-fA-F0-9]{8,}$", var.imagebuilder_ec2_subnet_id))
+    error_message = "Invalid Subnet ID. A valid Subnet ID must start with 'subnet-', followed by 8 digits"
+  }
 }
 
 variable "imagebuilder_ec2_terminate_on_failure" {
@@ -86,17 +118,39 @@ variable "imagebuilder_ec2_terminate_on_failure" {
 variable "imagebuilder_ec2_vpc_id" {
   type        = string
   description = "ID of the VPC used to host the temporary EC2 instance that will be created in order to generate the AMI"
+  validation {
+    condition     = can(regex("^vpc-[a-fA-F0-9]{8,}$", var.imagebuilder_ec2_vpc_id))
+    error_message = "Invalid VPC ID. A valid VPC ID must start with 'vpc-', followed by 8 digits"
+  }
+}
+
+variable "kms_deletion_window_in_days" {
+  type        = number
+  description = "The number of days to retain a KMS key scheduled for deletion. Defaults to 7"
+  default     = 7
+  validation {
+    condition     = var.kms_deletion_window_in_days == floor(var.kms_deletion_window_in_days)
+    error_message = "The variable must be a whole integer"
+  }
 }
 
 variable "permission_boundary_arn" {
   type        = string
   description = "ARN of the IAM Policy to use as a permission boundary for the AWS ImageBuilder Role created by this module. Defaults to empty (i.e. no permission boundary required)"
   default     = ""
+  validation {
+    condition     = can(regex("^(arn:aws:iam::[0-9]+:.*)?$", var.permission_boundary_arn))
+    error_message = "Invalid Amazon Resource Name. A valid IAM Policy ARN must start with 'arn:aws:iam', followed by a region, account ID and resource name separated by colons."
+  }
 }
 
 variable "region" {
   type        = string
   description = "The AWS region in which to create resources"
+  validation {
+    condition     = can(regex("^[a-z]+-[a-z]+-[0-9]+$", var.region))
+    error_message = "Invalid Amazon Region ID"
+  }
 }
 
 variable "resource_tags" {
@@ -108,6 +162,10 @@ variable "resource_tags" {
 variable "runner_account_id" {
   type        = string
   description = "The AWS account ID that should host the GitHub Runners"
+  validation {
+    condition     = can(regex("^[0-9]{12}$", var.runner_account_id))
+    error_message = "Invalid account id. A valid account ID must contain 12 digits"
+  }
 }
 
 variable "unique_prefix" {
